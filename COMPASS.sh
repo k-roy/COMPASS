598aa2d82b8639c33369864a1b39268fd7ba021b
k-roy <kevinrjroy@gmail.com>
2023-01-12 21:39:47 +0700

    Update COMPASS.sh

--- COMPASS.sh
+++ COMPASS.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+################################ HARD DRIVE SPACE WARNING ################################
+# The COMPASS directory needs to have substantial free space for the alignment files,
+# on the order of 10X the size of the fastq.gz files when run on many aligners. 
+# These can be deleted at the end of the pipeline for each sample to avoid accumulating excess storage.
+
 ##################### VARIABLE AND PATH DEFINITIONS BEGIN #####################
 NUM_THREADS=32 
 # Change to allowable number of threads for your system.
@@ -9,13 +14,29 @@ READS_TO_PROCESS=-1
 # Set to -1 for all reads after tests pass.
 
 # change $HOME variable to your desired directory
-COMPASS_DIR=$HOME"/COMPASS/"
+COMPASS_DIR="$HOME/COMPASS/"
 REFERENCE_DIR=$COMPASS_DIR"genome_references/"
-FASTA=$REFERENCE_DIR"S288C_reference_sequence_R64-2-1_20150113_reformatted_chromosome_names.fasta"
-GFF=$REFERENCE_DIR"S288C_reference_sequence_R64-2-1_20150113_reformatted_chromosome_names.gff"
-GTF=$REFERENCE_DIR"S288C_reference_sequence_R64-2-1_20150113_reformatted_chromosome_names.gtf"
+GENOME_VERSION="S288C_reference_sequence_R64-2-1_20150113_reformatted_chromosome_names"
+FASTA=$REFERENCE_DIR$GENOME_VERSION".fasta"
+GFF=$REFERENCE_DIR$GENOME_VERSION".gff"
+GTF=$REFERENCE_DIR$GENOME_VERSION".gtf"
+SAMFIXCIGAR="/oak/stanford/groups/larsms/kevinroy/scripts/jvarkit/dist/samfixcigar.jar"
+NUM_THREADS=16
+MIN_INTRON_LENGTH=20
+MAX_INTRON_LENGTH=2000 # typically set to 200,000 for human introns
+ACCESSION="NA"
+READ_LENGTH=75
+READS_TO_PROCESS=-1 # 1000000 # 
+# Subsample this many reads if SUBSAMPLE_READS is set to true.
+# set to -1 for all reads
+
+ALIGNERS_FILE=$COMPASS_DIR"sample_aligner_info.tsv"
+HISAT2_GENOME_DIR=$REFERENCE_DIR"HISAT2_annotated_index"
+
 INTRONS_FILE=$REFERENCE_DIR"saccharomyces_cerevisiae_R64-2-1_20150113_introns.tsv"
-ALIGNERS_FILE=$COMPASS_DIR"aligners.txt"
+
+# The sample_aligner_info file tells COMPASS which alignment programs to use.
+ALIGNERS_FILE=$COMPASS_DIR"sample_aligner_info.txt"
 
 ## ASSIGN AND CREATE SUBDIRECTORIES
 NUMBERED_READS_DIR=$COMPASS_DIR"numbered_reads_fastq/"
@@ -36,30 +57,60 @@ mkdir $LOG_DIR
 ###################### VARIABLE AND PATH DEFINITIONS END ######################
 
 conda activate compass
+
+# gffread is needed to convert the S. cerevisiae GFF file from SGD to GTF format
 gffread -T --force-exons --gene2exon $GFF -o $GTF $GFF
 
-####################### BATCH PROCESSING COMMANDS BEGIN #######################
-SAMPLE_SUFFIX="_name_sorted.bam"
+####################### SAMPLE PROCESSING COMMANDS BEGIN #######################
 
-# batch processing prp18/Roy et al.
-ACCESSION="PRJNA544962"
-READ_LENGTH=150
-for i in {9130287..9130292};
+echo "File name is "$0 # holds the current script
+echo "Sample name is "$1
+SAMPLE=$1
+SAMPLE_NAME=$SAMPLE
 
-do SAMPLE="SRR"$i
+if [[ $READS_TO_PROCESS -lt 1 ]];
+then
+echo "processing all reads for "$SAMPLE
+else
+echo "subsampling "$READS_TO_PROCESS" reads for "$SAMPLE
+SAMPLE=$SAMPLE"_subsampled"
+fi
 
-echo "READS_TO_PROCESS: "$READS_TO_PROCESS
-echo "starting process_reads_and_align.sh for "$SAMPLE;
-sh process_reads_and_align.sh -C "$COMPASS_DIR" -R "$REFERENCE_DIR" \
--S "$SAMPLE" -F "$FASTA" -G "$GTF" -P "$NUM_THREADS" \
--L $READ_LENGTH -Z $READS_TO_PROCESS \
-> $LOG_DIR$SAMPLE"_COMPASS_process_reads_and_align.log" 2>&1
+# echo "READS_TO_PROCESS: "$READS_TO_PROCESS
+# echo "starting process_reads_and_align.sh for "$SAMPLE_NAME;
+# sh process_reads_and_align.sh -C "$COMPASS_DIR" -R "$REFERENCE_DIR" \
+# -S "$SAMPLE_NAME" -F "$FASTA" -G "$GTF" -P $NUM_THREADS \
+# -M $MIN_INTRON_LENGTH -N $MAX_INTRON_LENGTH -A $SAMFIXCIGAR \
+# -L $READ_LENGTH -Z $READS_TO_PROCESS \
+# > $LOG_DIR$SAMPLE"_process_reads_and_align.log" 2>&1
 
-# echo "starting compare_splice_junctions_from_multiple_aligners.py for "$SAMPLE;
-# python -u compare_splice_junctions_from_multiple_aligners.py \
-# "$SEPARATE_ALIGNMENTS_DIR" "$OPTIMAL_ALIGNMENTS_DIR" "$FASTA" "$INTRONS_FILE" \
-# "$ALIGNERS_FILE" "$SAMPLE" "$SAMPLE_SUFFIX" "$READS_TO_PROCESS" \
-# > $LOG_DIR$SAMPLE"_COMPASS.log" 2>&1
+echo "starting compare_splice_junctions_from_multiple_aligners.py for "$SAMPLE;
+python compare_splice_junctions_from_multiple_aligners.py \
+"$COMPASS_DIR" "$REFERENCE_DIR" "$SAMPLE" "$FASTA" "$INTRONS_FILE" "$NUM_THREADS" \
+"$MIN_INTRON_LENGTH" "$MAX_INTRON_LENGTH" "$ALIGNERS_FILE" "$READS_TO_PROCESS" \
+> $LOG_DIR$SAMPLE"_compare_splice_junctions_from_multiple_aligners_sh_output.log" 2>&1
 
+echo "starting analyze_exonic_and_intronic_sequence.py for "$SAMPLE;
+python analyze_exonic_and_intronic_sequence.py \
+"$COMPASS_DIR" "$REFERENCE_DIR" "$SAMPLE" "$FASTA" "$INTRONS_FILE" "$NUM_THREADS" \
+"$MIN_INTRON_LENGTH" "$MAX_INTRON_LENGTH" "$ALIGNERS_FILE" "$READS_TO_PROCESS" \
+> $LOG_DIR$SAMPLE"_analyze_exonic_and_intronic_sequence_sh_output.log" 2>&1
+
+echo "starting create_splice_site_bed.py for "$SAMPLE;
+python create_splice_site_bed.py $COMPASS_JUNCTIONS_DIR $SAMPLE
+
+echo "starting add_unspliced_read_counts_to_junctions.py for "$SAMPLE;
+for strand in "plus" "minus";
+do echo $strand;
+for splice_site in "fiveSS" "threeSS";
+do echo $splice_site;
+BED="$COMPASS_JUNCTIONS_DIR""$SAMPLE""_"$strand"_strand_"$splice_site"_splice_site.bed"
+BAM="$OPTIMAL_ALIGNMENTS_DIR""$SAMPLE""_COMPASS_UGA_"$strand"_strand_sorted.bam"
+OUT="$COMPASS_JUNCTIONS_DIR""$SAMPLE""_"$strand"_strand_"$splice_site"_splice_site_coverage.bed"
+samtools depth -b $BED $BAM > $OUT 
 done
-####################### BATCH PROCESSING COMMANDS END #######################
+done
+
+python add_unspliced_read_counts_to_junctions.py $COMPASS_JUNCTIONS_DIR $SAMPLE
+
+####################### SAMPLE PROCESSING COMMANDS END #######################
